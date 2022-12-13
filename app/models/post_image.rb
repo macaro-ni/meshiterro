73b8b6b85fd6fcc408016ac5a54cbf73b4196f10
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2022-12-13 12:39:55 +0000

    [Add]-11章

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,4 @@
 class PostImage < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_one_attached :image
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :post_images
+
+resources :post_images, only: [:new, :create, :index, :show]
+
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about' , as: 'about'
