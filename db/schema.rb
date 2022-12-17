3ac8770c41ccdb3bd99f499ef78a70cba5ca2fbe
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2022-12-17 11:50:12 +0000

    エラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2022_12_15_015756) do
     t.string "shop_name"
     t.text "caption"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
